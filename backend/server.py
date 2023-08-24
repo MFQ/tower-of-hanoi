b459493e06715a4046e175df2c4a238bec5a84f1
Muhammad Fazil Qureshi <softmfq@gmail.com>
2023-08-24 20:05:59 +0400

    update: fixed production config error

--- backend/server.py
+++ backend/server.py
@@ -6,44 +6,25 @@ from flask_cors import CORS, cross_origin
 from tower_of_hanoi.algorithm import Algorithm
 
 
-def create_app(test_config=None):
+# def create_app():
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    
-    cors = CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+@cross_origin
+@app.route('/generate-steps', methods=["POST"])
+def generate_steps():
+    body = request.get_json()
+    if body.get("number_of_pegs"):
+        algo = Algorithm(body["number_of_pegs"])
+        steps = algo.generate_tower_of_hanoi()
+        return {
+            "number_of_pegs": body["number_of_pegs"],
+            "steps": steps
+        }
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    @cross_origin
-    @app.route('/generate-steps', methods=["POST"])
-    def generate_steps():
-        body = request.get_json()
-        if body.get("number_of_pegs"):
-            algo = Algorithm(body["number_of_pegs"])
-            steps = algo.generate_tower_of_hanoi()
-            return {
-                "number_of_pegs": body["number_of_pegs"],
-                "steps": steps
-            }
-        else:
-            return {
-                "error": "missing number of pegs from the body"
-            }
-
-    return app
+        return {
+            "error": "missing number of pegs from the body"
+        }
+    # return app
---
+++ backend/wsgi.py
@@ -0,0 +1,11 @@
+# from server import create_app
+from server import app
+# def server(arg:None, karg:None):
+#     print(arg)
+#     print(karg)
+#     app = create_app()
+#     app.run(debug=True)
+    
+
+if __name__ == '__main__':
+    app.run()
